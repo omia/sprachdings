ecb9d504a32922fa28f12464eb08c201cf885532
omia <omia@live.de>
2018-01-08 18:25:31 +0100

    Initial commit

---
+++ src/client/starter.java
@@ -0,0 +1,6 @@
+package client;
+
+public class starter {
+
+    public static void main(String arg[]){}
+}
---
+++ src/server/starter.java
@@ -0,0 +1,6 @@
+package server;
+
+public class starter {
+
+    public static void main(String arg[]){}
+}
