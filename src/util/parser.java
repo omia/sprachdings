eaee304f80f16f091223f81dbe21ba29d76d0c14
simonprein <simonprein@gmail.com>
2018-01-20 00:48:15 +0100

    hh

--- src/util/parser.java
+++ src/util/parser.java
@@ -3,37 +3,45 @@ package util;
 public class parser {
 
     public static void main(String[] args) {
-            System.out.println(countSeperators("as" + '#' + "df" + '#' + "gh" + '#'));
+
     } // end of main
 
+    public parser(){}
+
     public static String arrayToString(String[] a) {
-        String s = "";
-        for (int i = 1; i < a.length - 1; i++) s += a[i] + '#';
-        return s;
+        String s = "<begin>";
+        for (int i =0 ;i < a.length;i++) s += "<" + i + ">" + a[i] + "</" + i + ">";
+        s += "<end>";
+        return s ;
     }
 
-    private static int countSeperators(String a ){
-        String temp = a ;
-        int k = 0;
-        while (temp.indexOf('#') > 0){
-            k++;
-            System.out.println(temp.indexOf('#'));
-            temp = temp.substring( temp.indexOf('#')+1) ;
-            System.out.println(temp);
-            System.out.println(temp.indexOf('#'));
-        }
-        return k ;
+    private static  boolean Message_isComplete(String p){
+        if(p.indexOf("<begin>")!=1 &&  p.indexOf("<end>")!=1  ) return true;
+        return false;
+
     }
 
-
-    public static String[] StringtoArray(String a){
-       int seperators = countSeperators(a);
-       String[] t =  new String[seperators] ;
-       for(int i = 0 ; a.indexOf('#')>0; i++){
-           t[i] = a.substring(0,a.indexOf('#')) ;
+    public static  String[] StringtoArray(String a){
+        String[] outputString;
+        outputString = new String[1];
+        if(Message_isComplete(a)) {
+            int arguments_num = Integer.parseInt("" + a.charAt(a.indexOf("<end>") - 2)) + 1;
+            outputString = new String[arguments_num];
+            for (int i = 0; i < arguments_num; i++) {
+                outputString[i] = a.substring(a.indexOf("<" + i + ">") + 3, a.indexOf("</" + i + ">"));
+            }
+            return outputString;
         }
-        return t ;
+        return outputString;
+        }
 
-}
+    private static void teststringtoArray(){
+        String[] b = StringtoArray("<begin><0>a</0><1>d</1><2>a</2><end>");
+        for (int i = 0 ; i<b.length;i++){
+            System.out.println(b[i]);
+
+        }
+
+    }
 
 }
